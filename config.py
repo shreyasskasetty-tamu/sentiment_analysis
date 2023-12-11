2b288cb234c6e2cfcbcfa4a64e89060792e3690d
shreyasskasetty-tamu <142867885+shreyasskasetty-tamu@users.noreply.github.com>
2023-12-10 20:36:17 -0600

    add new changes to config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@ MAX_LEN = 256
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 16
 TEST_BATCH_SIZE = 32
-EPOCHS = 2
+EPOCHS = 10
 BERT_PATH = "bert-base-uncased"
 MODEL_PATH = "models/model.pt"
-TRAINING_FILE = "/content/drive/MyDrive/YelpDataset/yelp_review_train.csv"
-TEST_FILE = "/content/drive/MyDrive/YelpDataset/yelp_review_test.csv"
+TRAINING_FILE = "dataset/yelp_review_train.csv"
+TEST_FILE = "dataset/yelp_review_test.csv"
 PREPROCESSED_DATA_PATH = "dataset/preprocessed_yelp_review.csv"
-BALANCED_DATASET_PATH = "/content/drive/MyDrive/YelpDataset/new_balanced_yelp_review.csv"
+BALANCED_DATASET_PATH = "dataset/balanced_yelp_review_train.csv"
 BERT_TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
 ROBERTA_TOKENIZER =  transformers.RobertaTokenizer.from_pretrained('roberta-base')
 STOP_WORDS_DOWNLOAD_PATH = "dataset/"
